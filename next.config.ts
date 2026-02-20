822003a7535cadb582359f789a44a06e50801d92
Julie Vitkova <julievitkova@Julies-MacBook-Air.local>
2026-02-20 17:15:06 +0100

    repaired pictures on vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "glttpnvctscbccxtdhjq.supabase.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
